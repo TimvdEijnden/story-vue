3e914a9dd5322205a5543fb5eef7c703fe4f1457
Tim van den Eijnden <tvandeneijnden@libertyglobal.com>
2019-10-30 11:12:20 +0100

    Make app work again by updating firebase code

---
+++ README.md
@@ -0,0 +1,26 @@
+# Real-time Story
+The code is live at https://timvdeijnden.github.io/story-vue/
+
+## Using Vue.js & Firebase.
+
+I've created this experiment in 2014 to create the same app using different frameworks. Back then I've created it in
+- Vue.js
+- Ember.Js
+- Backbone.js
+- Angular
+
+And Firebase real-time database, which was the only Firebase product back then. It used version 1.0.10 of the Firebase dependency. The database was open for everyone to read and write.
+Also bower was used to manage dependencies.
+
+## Current version
+
+I've updated the code to make it work again, it now also uses the Firebase Cloud functions to save the data which is more secure since writing is limited now. It still uses the old Vue code & and old Bootstrap.
+
+To be specific:
+ - Vue.js v0.9.3 (2014)
+ - Bootstrap v3.0.3 (2013)
+ - Firebase: (7.2.2)
+     - Real-time Database
+     - Cloud functions
+
+For more info about me please visit: https://timvandeneijnden.nl.
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-var storyApp = new Vue({
+const cloudFunction = 'https://europe-west1-realtime-story.cloudfunctions.net/addSentence';
+
+const storyApp = new Vue({
     el: '#app',
     filters: {
-        formatSentence: function (val) {
+        formatSentence(val) {
             if(!val) return;
             var newText = val;
             if(['.','!','?',','].indexOf(newText.substr(-1)) == -1){
                 newText += '.';
             }
-            newText += ' ';
             newText = newText.substr(0,1).toUpperCase() + newText.substr(1);
             return newText;
         }
@@ -16,29 +17,38 @@ var storyApp = new Vue({
         sentences : []
     },
     methods: {
-        addSentence: function(e){
+        addSentence(e){
             e.preventDefault();
             if(this.$data.sentence_text.trim().length > 10){
-                Sentences.push({author: this.$data.sentence_author, text: this.$data.sentence_text.trim() });
+                const sentence = {author: this.$data.sentence_author, text: this.$data.sentence_text.trim() }
                 this.$data.sentence_text= "";
+                fetch(cloudFunction, {
+                    method: 'post',
+                    body: JSON.stringify(sentence),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).catch((error) => {
+                    alert(`Something went wrong: ${error}.`);
+                });
             }else{
                 alert('Please write a correct sentence');
-            }  
+            }
         }
     }
 });
+const db = firebase.database();
+const Sentences = db.ref('sentences');
 
-var Sentences = new Firebase("https://burning-fire-5298.firebaseio.com");
-
-Sentences.on('child_added', function (snapshot) {
+Sentences.on('child_added', (snapshot) => {
     var item = snapshot.val();
-    item.id = snapshot.name();
+    item.id = snapshot.key;
     storyApp.sentences.push(item);
 })
 
-Sentences.on('child_removed', function (snapshot) {
-    var id = snapshot.name();
-    storyApp.sentences.some(function (sentence) {
+Sentences.on('child_removed',(snapshot) => {
+    var id = snapshot.key();
+    storyApp.sentences.some(sentence => {
         if (sentence.id === id) {
             storyApp.sentences.remove(sentence)
             return true
